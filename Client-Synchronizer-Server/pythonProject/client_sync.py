6d5f7f7d943d4858787e10cf7b7fb7b70e184326
Rekongstor <rekongstor@mail.ru>
2024-10-31 20:01:37 +0300

    Updated synchronizer

--- Client-Synchronizer-Server/pythonProject/client_sync.py
+++ Client-Synchronizer-Server/pythonProject/client_sync.py
@@ -4,10 +4,28 @@ from datetime import datetime as dt
 import requests
 import threading
 
-service_port_global = 36020
+import logging
+import sys
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.DEBUG)
+stdout_handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler('sync_log.txt')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stdout_handler)
+
+service_port_global = 37020
 service_port_local = 510
-timeToKillPublicSession = 7200  # 2 hours
-timeToKillTestSession = 30  # test session - 30 seconds
+timeToKillPublicSession = 7200 # 2 hours
+timeToKillTestSession = 30 # test session - 30 seconds
 
 lastTimeCheck = dt.now()
 
@@ -19,9 +37,10 @@ app = Flask(__name__)
 @app.route('/api', methods=['POST'])
 def api():
     print(request.data)
+    logger.info(request.data)
     reqData = request.data.decode(encoding='cp1251', errors='ignore')
     if request.method == 'POST':
-        # reqJson = request.get_json()
+        #reqJson = request.get_json()
         reqJson = json.loads(reqData, strict=False)
         method = str(reqJson["method"])
         data = reqJson["data"]
@@ -34,11 +53,10 @@ def api():
             return jsonify(response)
 
         if method == 'getDataUsers':
-            reqJson = json.loads(request.data.decode(encoding='utf-8', errors='ignore'),
-                                 strict=False)  # for some reason encoding is not valid here
+            reqJson = json.loads(request.data.decode(encoding='utf-8', errors='ignore'), strict=False) # for some reason encoding is not valid here
             data = reqJson["data"]
             sessionToken = reqJson["sessionToken"]
-            if sessionToken in activeSessionTokens:  # if session exists - request or load usersTalents
+            if sessionToken in activeSessionTokens: # if session exists - request or load usersTalents
                 tloc = threading.local()
                 session = activeSessionTokens[sessionToken]
                 if 'usersData' in session:
@@ -49,58 +67,56 @@ def api():
             else:
                 return "No session exists"
 
+
         if method == "registerUserInSession":
             def checkOldSessions():
-                # fast check available sessions to kill
+                #fast check available sessions to kill
                 for oldSessionToken in list(activeSessionTokens.keys()):
                     oldSession = activeSessionTokens[oldSessionToken]
                     targetTimeDifferenceToKill = timeToKillPublicSession
                     if oldSessionToken == 'testSessionToken':
                         targetTimeDifferenceToKill = timeToKillTestSession
                     if (dt.now() - oldSession['timestamp']).total_seconds() > targetTimeDifferenceToKill:
-                        del activeSessionTokens[oldSessionToken]  # remove old sessions
+                        del activeSessionTokens[oldSessionToken] # remove old sessions
 
             checkOldSessions()
-
+            
             sessionToken = data["sessionToken"]
             if sessionToken not in activeSessionTokens:
                 tloc = threading.local()
                 if sessionToken not in activeSessionTokens:
-                    activeSessionTokens[sessionToken] = {'token': sessionToken, 'gameName': '', 'players': [
-                        {'nickname': data["nickname"], 'heroId': data["heroId"]}], 'timestamp': dt.now()}
+                    activeSessionTokens[sessionToken] = {'token': sessionToken, 'gameName': '', 'players': [ { 'nickname': data["nickname"], 'heroId': data["heroId"] } ], 'timestamp': dt.now()}
                     response = {
                         'error': '',
                         'data': ''
                     }
                     return jsonify(response)
-
-            if sessionToken in activeSessionTokens:  # if session exists - try to add new player
+                
+            if sessionToken in activeSessionTokens: # if session exists - try to add new player
                 session = activeSessionTokens[sessionToken]
 
-                if session['gameName'] == '':  # wait for creator
+                if session['gameName'] == '': # wait for creator
                     response = {
                         'error': 'Wait',
                         'data': ''
                     }
                     return jsonify(response)
-                else:  # lobby was successfully created
+                else: # lobby was successfully created
                     for player in activeSessionTokens[sessionToken]['players']:
-                        if player['nickname'] == data["nickname"]:  # player already registered in this session
+                        if player['nickname'] == data["nickname"]: # player already registered in this session
                             response = {
                                 'error': 'PlayerAlreadyRegistered',
                                 'data': ''
                             }
                             return jsonify(response)
-                    activeSessionTokens[sessionToken]['players'].append(
-                        {'nickname': data["nickname"], 'heroId': data["heroId"]})
+                    activeSessionTokens[sessionToken]['players'].append({ 'nickname': data["nickname"], 'heroId': data["heroId"] })
                     response = {
                         'error': '',
                         'data': session['gameName']
                     }
                     return jsonify(response)
-            else:  # if session does not exist - create one
-                activeSessionTokens[sessionToken] = {'token': sessionToken, 'gameName': '', 'players': [
-                    {'nickname': data["nickname"], 'heroId': data["heroId"]}], 'timestamp': dt.now()}
+            else: # if session does not exist - create one
+                activeSessionTokens[sessionToken] = {'token': sessionToken, 'gameName': '', 'players': [ { 'nickname': data["nickname"], 'heroId': data["heroId"] } ], 'timestamp': dt.now()}
                 response = {
                     'error': '',
                     'data': ''
@@ -113,8 +129,7 @@ def api():
                 session = activeSessionTokens[sessionToken]
 
                 if session['gameName'] == '':  # save gameId
-                    activeSessionTokens[sessionToken]['gameName'] = data[
-                        "nickname"]  # just save creator's nickname for now
+                    activeSessionTokens[sessionToken]['gameName'] = data["nickname"] # just save creator's nickname for now
                     response = {
                         'error': '',
                         'data': 0
@@ -130,6 +145,7 @@ def api():
             if sessionToken in activeSessionTokens:
                 session = activeSessionTokens[sessionToken]
 
+                logger.info(sessionToken + ' players ready: ' + str(len(session['players'])))
                 if len(session['players']) < 10:  # not enough players
                     response = {
                         'error': '',
@@ -149,8 +165,9 @@ def api():
             sessionToken = data["sessionToken"]
             if sessionToken in activeSessionTokens:
                 session = activeSessionTokens[sessionToken]
-
-                if 'usersData' in session:
+                
+                if 'usersData' not in session:
+                    activeSessionTokens[sessionToken]['players'].append({ 'nickname': 'FakeReconnect', 'heroId': 'FakeHero' })
                     response = {
                         'error': 'Connect',
                         'data': session['gameName']
@@ -169,6 +186,6 @@ def api():
     else:
         return 'Request method is not allowed'
 
-
 if __name__ == '__main__':
     app.run(debug=True, port=service_port_global + service_port_local, host="127.0.0.1", threaded=True)
+    
